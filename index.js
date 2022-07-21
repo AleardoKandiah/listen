0e40d18c21c6c28524ebc76eb155a96f1929cce8
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-07-21 18:26:24 +0100

    commands setup

--- index.js
+++ index.js
@@ -8,3 +8,32 @@ const { Player } = require("discord-player") //help manage quering in songs
 
 dotenv.config
 const TOKEN = process.env.TOKEN
+
+// load command allow script initialization with boolean intrepretation 
+const LOAD_SLASH = process.argv[2] == "load"
+
+const CLIENT_ID = "999620563930722314"
+const GUILD_ID = "918637381341548554"
+
+// initialise discord client
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_VOICE_STATES"
+    ]
+})
+
+client.slashcommands = new Discord.Collection()
+// YouTube downloader to handle music streaming
+client.player = new Player(client, {
+    ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25
+    }
+})
+
+// create a slash commands loader
+
+let commands = []
+
+const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"))
