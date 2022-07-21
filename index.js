6d2d8e2ab2a24606233b062a7a2d2278857bf766
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-07-21 15:14:53 +0100

    imports

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const Discord = require("discord.js")
 const dotenv = require("dotenv")
 const { REST } = require("discordjs/rest")
+const { Routes } = require("discord-api-tyoes/v9") //REST and routes for slash commands
+const fs = require("fs") //file integration 
+const { Player } = require("discord-player") //help manage quering in song
+
 
 dotenv.config
 const TOKEN = process.env.TOKEN
