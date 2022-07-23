ac0b307b845d451430b8f885baff58139f0f7136
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-07-23 20:20:38 +0100

    bot VC connection

--- slash/play.js
+++ slash/play.js
@@ -9,18 +9,32 @@ module.exports = {
         .addSubcommand((subcommand) =>
             subcommand.setName("Song")
             .setDescription("Loads a single song from url")
-            .addStringOption((option) => option.setName("url").setDescription("The song's url").setRequired(true))
+            .addStringOption((option) => option.setName("url").setDescription("The song's url").setRequired(true)
+            )
         )
         // subcmd that loads whole playlisit,
         .addSubcommand((subcommand) =>
         subcommand
             .setName("playlist")
             .setDescription("Loads a playlist of songs from a URL ")
-            .addStringOption((option) => option.setName("url").setDescription("The playlist's url").setRequired(true))
+            .addStringOption((option) => option.setName("url").setDescription("The playlist's url").setRequired(true)
+            )
         )
         //search search cmd via keywords
         .addSubcommand((subcommand)=>
             subcommand.setName("search").setDescription("Searches of song based on provided keyword(s)")
-            .addStringOption((option) => option.setName("search terms").setDescription("the search keyword").setRequired(true))
-        )
+            .addStringOption((option) => option.setName("search terms").setDescription("the search keyword").setRequired(true)
+            )
+        ),
+        run: async ({ client, interaction }) => {
+            // check if user is in a VC
+            if (!interaction.member.voice.channel)
+            return interaction.editReply("You need to be in a voice channel to use this command")
+            // else create a queue
+            const queue = await client.player.createQueue(interaction.guild)
+            // when there is no queue add bot to VC 
+            if (!queue.connection) await queue.connection(interaction.member.voice.channel)
+
+            let embed = new MessageEmbed()
+        }
 }
