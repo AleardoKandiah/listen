b0fb757b72bae4cc9db4acb60570763749e7280a
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-07-23 11:30:33 +0100

    slash commad export component

--- slash/play.js
+++ slash/play.js
@@ -1,3 +1,13 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
 const { QueryType } = require("discord-player")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("play")
+        .setDescription("Loads songs from YouTube")
+        .addSubcommand((subcommand) =>
+            subcommand.setName("Song")
+            .setDescription("Loads a single song from URL")
+            .addStringOption((option) => option.setName("URL").setDescription("The song's URL").setRequired(true)))
+}
