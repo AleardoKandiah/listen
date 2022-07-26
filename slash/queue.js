586c5d8c5d008bdac064252349e60bd3f3649036
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-07-26 21:03:36 +0100

    queue commands

--- slash/queue.js
+++ slash/queue.js
@@ -6,5 +6,12 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName("queue")
     .setDescription("displays the current song queue")
-    .addNumberOption((option) => option.setName("page").setDescription("Page nunber of queue").setMinValue(1))
+    .addNumberOption((option) => option.setName("page").setDescription("Page nunber of queue").setMinValue(1)),
+
+    run: async ({ client, interaction }) => {
+        const queue = client.player.getQueue(interaction.guildId)
+        if (!queue || !queue.playing){
+            return await interaction.editReplu("There are no songs in the queue")
+        }
+    }
 }
