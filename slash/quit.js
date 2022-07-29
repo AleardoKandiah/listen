31f5eb7c5f25d3f829bc9ddc4aabf0ecd87bd923
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-07-29 12:06:21 +0100

    quit command

--- slash/quit.js
+++ slash/quit.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
-const { QueryType } = require("discord-player")
 
 module.exports = {
     data: new SlashCommandBuilder()
