5f8d1c92b80a16ce796123a8916d555c9f2247a6
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-07-29 12:22:16 +0100

    quit command

--- slash/quit.js
+++ slash/quit.js
@@ -8,5 +8,10 @@ module.exports = {
 
     run: async ({client, interaction}) => {
         const queue = client.player.getQueue(interaction.guildId)
+        
+        if (!queue) return await interaction.editReply("There are no songs in the queue")
+
+        queue.destroy()
+        await interaction.editReply("Bye!")
     }
 }
