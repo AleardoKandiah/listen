4667791ebd32ae24622de472422d1da217210cae
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-07-30 12:09:56 +0100

    info command

--- slash/info.js
+++ slash/info.js
@@ -9,8 +9,8 @@ module.exports = {
         const queue = client.player.getQueue(interaction.guildId)
         
         if (!queue) return await interaction.editReply("There are no songs in the queue")
-
-        queue.shuffle()
+ 
+        // create a progress bar
         await interaction.editReply(`The queue of ${queue.tracks.length} songs have been shuffled!`)
     }
 }
