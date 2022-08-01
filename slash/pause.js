d48135e749b9b69324ef758cc04e83f3e80d9418
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-08-01 18:05:21 +0100

    pause command

--- slash/pause.js
+++ slash/pause.js
@@ -10,7 +10,7 @@ module.exports = {
         
         if (!queue) return await interaction.editReply("There are no songs in the queue")
 
-        queue.destroy()
+        queue.setPaused(true)
         await interaction.editReply("Bye!")
     }
 }
