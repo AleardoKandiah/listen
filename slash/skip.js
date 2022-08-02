ab6cdfec2ee3b2754df1e1a0ba5accb81b53c103
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-08-02 20:21:24 +0100

    skip cmd

--- slash/skip.js
+++ slash/skip.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder } = require("@discordjs/builders")
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("quit")
-        .setDescription("stops the bot and clears the queue"),
+        .setName("skip")
+        .setDescription("Skips the music"),
 
     run: async ({client, interaction}) => {
         const queue = client.player.getQueue(interaction.guildId)
